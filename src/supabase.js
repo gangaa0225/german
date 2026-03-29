d5bd5b5b9f0743f62a9cb2daf6fc5b1d2ba325d2
gangaa0225 <122040139+gangaa0225@users.noreply.github.com>
2026-03-29 04:36:18 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "YOUR_PROJECT_URL",
-  "YOUR_PUBLISHABLE_KEY"
+  "https://twdttpbkzjnqhkqvwnaj.supabase.co",
+  "sb_publishable_XMcVfZfx0y1KQw7hR1U7lg_n39T60aq"
 );
